e9ce5ac7ae3b10212353859cdc15fe0d8ccceed9
John Tian <85849926+snugpenguin968@users.noreply.github.com>
2025-11-15 14:45:23 -0600

    update dependencies

--- server/database.py
+++ server/database.py
@@ -12,7 +12,7 @@ HOST = os.getenv("host")
 PORT = os.getenv("port")
 DBNAME = os.getenv("dbname")
 
-DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
+DATABASE_URL = f"postgresql+psycopg://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
 engine = create_engine(DATABASE_URL, poolclass=NullPool)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
