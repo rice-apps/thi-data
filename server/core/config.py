adf8ed4edb269f32946cf2da4b55d9c2d13adae4
haotianshan007-4444 <haotianshan@outlook.com>
2026-01-31 15:36:13 -0600

    added celery worker to docker-compose, updated centralized Setting class, created API endpoint to trigger celery worker
    Co-authored-by: fmkde <fmkde@users.noreply.github.com>
    Co-authored-by: Teagan | Tiggen <tiggentime@users.noreply.github.com>

--- server/core/config.py
+++ server/core/config.py
@@ -10,10 +10,12 @@ class Settings(BaseModel):
     HOST: str = os.getenv("host", "localhost")
     PORT: str = os.getenv("port", "5432")
     DBNAME: str = os.getenv("dbname", "postgres")
+    BROKER_URL: str = os.getenv("broker_url", "pyamqp://guest@localhost//")
+    ORIGIN_URL: str = os.getenv("origin_url", "http://localhost:3000")
     
     @property
     def DATABASE_URL(self) -> str:
-        db_url = f"postgresql+psycopg://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DBNAME}?sslmode=require"
+        db_url = f"postgresql+psycopg2://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DBNAME}?sslmode=require"
         print(f"DB_URL = {db_url}")
         return db_url
 
